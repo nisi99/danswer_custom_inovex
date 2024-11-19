f016c04986bd86a946b99c4192c5288357bfa16d
Denise Hartmann <denise.hartmann@inovex.de>
2024-11-19 16:08:41 +0100

    rename env vars for confluence image summarization

--- backend/danswer/configs/chat_configs.py
+++ backend/danswer/configs/chat_configs.py
@@ -104,10 +104,14 @@ VESPA_SEARCHER_THREADS = int(os.environ.get("VESPA_SEARCHER_THREADS") or 2)
 # Summarize the content of the following image and be as precise as possible.
 # The summary will be embedded and used to retrieve the original image.
 # Therefore, write a concise summary of the image that is optimized for retrieval.""""
-SYSTEM_PROMPT = os.environ.get("SYSTEM_PROMPT") or None
+CONFLUENCE_IMAGE_SUMMARIZATION_SYSTEM_PROMPT = (
+    os.environ.get("CONFLUENCE_IMAGE_SUMMARIZATION_SYSTEM_PROMPT") or None
+)
 
 # Custom User Prompt for Image Summarization
 # Results may be better if the prompt is in the language of the main language of the source
 # Default prompt:
 # """Summarize the content and the subject of the picture."""
-USER_PROMPT = os.environ.get("USER_PROMPT") or None
+CONFLUENCE_IMAGE_SUMMARIZATION_USER_PROMPT = (
+    os.environ.get("CONFLUENCE_IMAGE_SUMMARIZATION_USER_PROMPT") or None
+)
